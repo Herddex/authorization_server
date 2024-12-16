de0ca5ad0f261c332a605de95feb3bb17acbc875
Herddex <jingatudor474@gmail.com>
2024-12-16 22:28:47 +0200

    Initial commit

---
+++ src/main/java/ro/ubb/rest_security_example_authorization_server/RestSecurityExampleAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package ro.ubb.rest_security_example_authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSecurityExampleAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSecurityExampleAuthorizationServerApplication.class, args);
+    }
+
+}
